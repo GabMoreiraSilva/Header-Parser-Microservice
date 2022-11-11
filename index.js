c1471ff602a75dc78c482e759263ca086ceae93a
Gabriel <53486357+GabMoreiraSilva@users.noreply.github.com>
2022-11-11 15:17:08 -0300

    projeto finalizado

---
+++ index.js
@@ -0,0 +1,42 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+app.set('trust proxy', true)
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use("/public", express.static(__dirname + "/public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  var lang;
+  var ip;
+  var browser;
+
+  ip = req.ip;
+  lang = req.headers["accept-language"];
+  agent = req.get('User-Agent');
+  res.send({ipaddress : ip, language: lang, software: agent})
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
